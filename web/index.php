eb87a98974e2c4d2675e0cf2455efb400a84a7b4
Eugene Pavlov <fox.sawyer@gmail.com>
2015-03-11 13:30:01 +0300

    Render xml

--- web/index.php
+++ web/index.php
@@ -3,11 +3,20 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
+$app['debug'] = true;
 $filename = 'todos.xml';
 $todos = simplexml_load_file($filename);
 
-$app->get('/', function () use ($app) {
-    return 'Hello, Silex ';
+$app->get('/', function () use ($todos) {
+	$output = '<ul>';
+	foreach ($todos->item as $item)
+	{
+		$output .= '<li>' . $item->text;
+		if ($item->done == 1) $output .= ' (выполнено)';
+		$output .= '</li>';
+	}
+	$output .= '</ul>';
+	return $output;
 });
 
 $app->run();
